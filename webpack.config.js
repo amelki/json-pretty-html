442f1848f990253d8e31a287015d640d78392934
Antoine Melki <amelki156@gmail.com>
2018-02-11 18:44:53 +0100

    Update dependencies Expose compiled bundle instead of direct sources

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js',
-    libraryTarget: 'var',
+    libraryTarget: 'umd',
     library: 'prettyPrint'
   },
   module: {
